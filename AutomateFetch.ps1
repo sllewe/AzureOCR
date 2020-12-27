1cd3f79ea4b343135e056f2244301bdcf371d4b0
sllewe <sllewe@gmail.com>
2020-12-27 16:59:09 -0500

    Update AutomateFetch.ps1

--- AutomateFetch.ps1
+++ AutomateFetch.ps1
@@ -1,1 +1,4 @@
-#comment
+#grab jpeg from IP Camera
+$output = Invoke-WebRequest -Uri 'http://192.168.1.187:80/snapshot.cgi?' -Credential $cred
+$cred = Get-Credential
+$file = Invoke-WebRequest -Uri 'http://192.168.1.187/snap.htm' | Out-File C:\Temp\snap.jpg
